ccf4a5544c75b770ff8832a5c4c9dfb00d337b00
mikha <mikhail.p2005@gmail.com>
2024-11-06 21:24:21 +0300

    kuku

--- app/src/main/java/com/example/laba3_part_2/GroupmatesActivity.java
+++ app/src/main/java/com/example/laba3_part_2/GroupmatesActivity.java
@@ -1,5 +1,6 @@
 package com.example.laba3_part_2;
 
+import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -21,10 +22,10 @@ public class GroupmatesActivity extends AppCompatActivity {
         StringBuilder groupmatesList = new StringBuilder();
 
         while (cursor.moveToNext()) {
-            String lastName = cursor.getString(cursor.getColumnIndex("last_name"));
-            String firstName = cursor.getString(cursor.getColumnIndex("first_name"));
-            String middleName = cursor.getString(cursor.getColumnIndex("middle_name"));
-            String timestamp = cursor.getString(cursor.getColumnIndex("timestamp"));
+            @SuppressLint("Range") String lastName = cursor.getString(cursor.getColumnIndex("last_name"));
+            @SuppressLint("Range") String firstName = cursor.getString(cursor.getColumnIndex("first_name"));
+            @SuppressLint("Range") String middleName = cursor.getString(cursor.getColumnIndex("middle_name"));
+            @SuppressLint("Range") String timestamp = cursor.getString(cursor.getColumnIndex("timestamp"));
 
             groupmatesList.append(lastName).append(" ").append(firstName).append(" ").append(middleName)
                     .append(" - ").append(timestamp).append("n");
